067056b539f5ea38b235944fc6ce589445dc8b04
Valera Makarov <valeramakarov@live.ie>
2023-08-08 17:03:30 +0100

    Add event listene to the button

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/** 
+ * Declare constants for DOM elements
+ * and possible choices 
+*/
+
+const buttons = document.getElementsByClassName('control');
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+const playerImage = document.getElementById('player-image');
+const computerImage = document.getElementById('computer-image');
+const message = document.getElementById('message');
+const choices  = ['rock', 'paper', 'scissers'];
