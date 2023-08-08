8db411eff5f584c99557b42f9234b49bfac233b4
Valera Makarov <valeramakarov@live.ie>
2023-08-08 17:06:34 +0100

    Add game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,15 @@ const playerImage = document.getElementById('player-image');
 const computerImage = document.getElementById('computer-image');
 const message = document.getElementById('message');
 const choices  = ['rock', 'paper', 'scissers'];
+
+/**
+ * Add event listener to all the button
+ */
+
+for (let button of buttons) {
+    console.log(button, 'button')
+    button.addEventListener('click', function() {
+        let playerChoice = this.getAttribute('data-choice');
+        playerGame(playerChoice);
+    });
+}
